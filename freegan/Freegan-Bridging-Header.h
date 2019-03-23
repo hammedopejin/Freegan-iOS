0f3beebbe79437fb7d0af4f89e8170144506a6d0
Hammed Opejin <hammedopejin@yahoo.com>
2019-03-23 14:00:15 -0700

    feat: Create RecentChat scene

---
+++ freegan/Freegan-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Freegan-Bridging-Header.h
+//  freegan
+//
+//  Created by Hammed opejin on 3/23/19.
+//  Copyright © 2019 Hammed opejin. All rights reserved.
+//
+
+#import <JSQMessagesViewController/JSQMessages.h>
